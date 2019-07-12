66b52d10ba54fca540b96585789fa1677febc76d
remvord <dv@mc-service.ru>
2019-07-12 17:34:18 +0300

    time

---
+++ arrays.py
@@ -0,0 +1,58 @@
+if __name__ == '__main__':
+    # s = 'hello world'
+    # for r in s:
+    #     print(r)
+    # s1 = s[0]
+    # print("-------")
+    # # print(s1)
+    # # s[0] = 'H'
+    # s2 = s[0:]
+    # # s3 = s[:]
+    # s3 = s[::-1]
+    #
+    # print('-----' * 10)
+    # s = ['1111', '2222', '33333']
+    # for r in s:
+    #     print(r)
+    #     for r1 in r:
+    #         print(r1)
+    # print('-' * 20)
+    # # s = [1, 2.5, 'wwwww', True]
+    # # print(s)
+    # # for r in s:
+    # #     print(type(r))
+    # # print(s.index(2.5))
+    # # if 2.6 in s:
+    # #     print('Ok')
+    # # else:
+    # #     print('Not found')
+    # # print(2.6 in s)
+    # print(s is None)
+    # print(type(s))
+    # s.append(1)
+    # print(len)
+    # print(len(s))
+    # s.insert(0, 9)
+    # s.append(6);
+    # s.append(5);
+    # s.append(7);
+    # s.append(2)
+    # # print(s)
+    # # s.sort()
+    # # s.reverse()
+    # print(s)
+    # s1 = reversed(s)
+    # print(reversed(s))
+    # for r in s1:
+    #     print(r)
+    # print(type(s1))
+    # s2 = list(s1)
+    # print(s2)
+    r = range(20, 10, -2)
+    print(r)
+    o = list(r)
+    print(o)
+    for q in r:
+        print(q)
+        q = 15
+        print(q)
--- mytime.py
+++ mytime.py
@@ -1,5 +1,11 @@
-import time
+import time, datetime
 
 if __name__ == '__main__':
     print(time.gmtime(0))
-    print(time.time())
+    print(type(time.time()))
+    t = time.time()
+    t1 = t+60*60*24*10
+    print(t1)
+    print(time.gmtime(t1))
+    dd = datetime.datetime.fromtimestamp(t1)
+    print(dd)
