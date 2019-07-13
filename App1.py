f75111b1ed6d428a252b168705bd9a5045e11349
remvord <rem-den@yandex.ru>
2019-07-13 20:28:27 +0300

    dell_day2

--- App1.py
+++ App1.py
@@ -1,3 +1,6 @@
 import funclib
 
 print(funclib.YES)
+print(funclib.__NO)
+print(funclib._CANCEL)
+print(funclib.f1('2'))
--- funclib.py
+++ funclib.py
@@ -1,2 +1,17 @@
-print('hello')
-YES='yes'
+# print('hello')
+YES = 'Yes'
+__NO = 'No'
+_CANCEL = 'Finish'
+
+
+def f1(n):
+    try:
+        # n = n*1
+        return n * n
+    except TypeError as e:
+        print(e)
+        raise e
+
+
+if __name__ == '__main__':
+    pass
