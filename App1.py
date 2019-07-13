4c0235ac612ea90a91e686fb9e5008f897fce70a
remvord <rem-den@yandex.ru>
2019-07-13 20:54:31 +0300

    dell_day2

--- App1.py
+++ App1.py
@@ -1,6 +1,9 @@
 import funclib
+from funclib import f3
 
-print(funclib.YES)
-print(funclib.__NO)
-print(funclib._CANCEL)
-print(funclib.f1('2'))
+# print(funclib.YES)
+# print(funclib.__NO)
+# print(funclib._CANCEL)
+# print(funclib.f1(2.1))
+# print(funclib.f2(n3=22,n2=1,n1=2,k=1))
+f3([1, 2, 3])
--- funclib.py
+++ funclib.py
@@ -7,10 +7,24 @@ _CANCEL = 'Finish'
 def f1(n):
     try:
         # n = n*1
-        return n * n
+        if type(n) is str:
+            return 'concat ' + (n + n)
+        if type(n) is int:
+            return n + n
+        raise TypeError
+
     except TypeError as e:
         print(e)
         raise e
+
+
+def f2(k, n1=10, n2=100, n3=300):
+    return k+n1+n2+n3
+
+
+def f3(k):
+    for r in k:
+        print(r)
 
 
 if __name__ == '__main__':
