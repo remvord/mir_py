b5cb31c3f8c7bf745f1d1e46aca6d1bab84e97c4
remvord <dv@mc-service.ru>
2019-07-15 13:39:52 +0300

    age

--- person.py
+++ person.py
@@ -1,9 +1,8 @@
 class Person():
-    # __fn = None
-
-    def __init__(self, fn, ln):
+    def __init__(self, fn, ln, age=0):
         self.__fn = fn
         self.__ln = ln
+        self.__age = age
 
     @property
     def fn(self):
@@ -13,7 +12,30 @@ class Person():
     def ln(self):
         return self.__ln
 
+    @property
+    def ln(self):
+        return self.__ln
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+    @age.getter
+    def getAge(self):
+        if self.__age == 0:
+            raise ValueError()
+        return self.__age
+
+    def __str__(self):
+        return f'{self.fn} {self.ln} возраст {self.age}'
+
 
 if __name__ == '__main__':
     p = Person('Ivan', 'Ivanov')
-    print(p.fn, p.ln)
+    p.age = 25
+    print(p.fn, p.ln, p.age)
+    print(p)
